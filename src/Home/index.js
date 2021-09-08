b8a2d86c947231ec79187831e6f5a75e8806d1a0
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-09-08 12:02:26 +0100

    made products unique

--- src/Home/index.js
+++ src/Home/index.js
@@ -11,51 +11,55 @@ function Home() {
           alt="home"
         />
         <div className="home__row">
-          <Product 
-            id={1}
-            title="The lean startupd da gaggde"
-            price={29.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-            rating={5}
-          />
-          <Product 
-            id={2}
-            title="The lean startup"
-            price={40.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-            rating={3}
-          />
-        </div>
-
-        <div className="home__row">
-          <Product
-              id={3}
-              title="The lean startup"
-              price={29.99}
-              image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-              rating={5}
-          />
           <Product
             id={4}
-            title="The lean startup"
-            price={29.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.09}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
             rating={5}
           />
           <Product
-            id={5}
-            title="The lean startup"
+            id={2}
+            title="The Lean Startup: How Constant Innovation Create Radically Successful Businesses Paperback"
             price={29.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL.SX325_B01,204,203,200_.jpg"
             rating={5}
           />
         </div>
 
         <div className="home__row">
+          <Product
+            id={3}
+            title="Samsung LC49RG90SSUXEN 49 Curve Led Gaming Monitor"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            rating={3}
+          />
+
+          <Product
+            id={5}
+            title="New Apple iPad Pro (12.9-inch, Wi-fi, 128GB) - Siver (4th Generation)"
+            price={598.00}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+            rating={5}
+          />
+
           <Product 
-            title="The lean startup afdaf dafg gea hd"
-            price={29.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            id={1}
+            title="Kenwood kMix Stand Miser for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk"
+            price={229.0}
+            image="https://st.depositphotos.com/1765561/4857/i/450/depositphotos_48579839-stock-photo-opened-blue-stand-mixer.jpg"
+            rating={4}
+          />
+
+        </div>
+
+        <div className="home__row">
+          <Product 
+            id={6}
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual QHD 5120 x 1440"
+            price={1094.98}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
             rating={4}
           />
         </div>
